258c02df530913053a691e366e7d29b391e78201
HAREESH1817 <yhareesh.1999@gmail.com>
2021-03-15 10:48:58 +0530

    updated file

--- private.py
+++ private.py
@@ -2,3 +2,5 @@ hii
 this is hareesh
 i am staying in banglore
 this is the modified model of the code
+this is the modifier code of the program at the merge model
+it execute the one branch to another branch
