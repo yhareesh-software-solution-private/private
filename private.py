fcccf43f1615c84ab4909bfb3886302fa2fc3b14
HAREESH1817 <yhareesh.1999@gmail.com>
2021-03-15 22:17:26 +0530

    updated

--- private.py
+++ private.py
@@ -4,3 +4,4 @@ i am staying in banglore
 this is the modified model of the code
 this is the modifier code of the program at the merge model
 it execute the one branch to another branch
+git stash command is used
